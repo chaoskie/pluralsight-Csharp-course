b0fe7e8ccc3794eaef7058afac9f240bc609eac4
Lars Blom <lars.blom@synerscope.nl>
2020-02-25 17:14:31 +0100

    6.5 vars can point to same objects test

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -13,7 +13,19 @@ namespace GradeBook.Tests
 
             Assert.Equal("book 1", book1.Name());
             Assert.Equal("book 2", book2.Name());
+            Assert.NotSame(book1,book2);
+        }
 
+        [Fact]
+        public void SameReferenceObjectsTest()
+        {
+            Book book1 = GetBook("book 1");
+            Book book2 = book1;
+
+            Assert.Equal("book 1", book1.Name());
+            Assert.Equal("book 1", book2.Name());
+            Assert.Same(book1,book2);
+            Assert.True(Object.ReferenceEquals(book1,book2));
         }
 
         Book GetBook(string name){
